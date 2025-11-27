4e80e2c0cc0f3f9bc58239cb220bbc1e81a2cb8b
Amaanhasan2003 <amaan.hasan25@gcu.edu.in>
2025-11-27 16:35:22 +0530

    Update ElseIfDemo.java

--- Source/ElseIfDemo.java
+++ Source/ElseIfDemo.java
@@ -1,15 +1,15 @@
-class ElseIfDemo {
-public static void main(String[] args) {
-int temperature = 25;
 
-Programs to Demonstrate Conditional Statements in
-Java
+public class ElseIfDemo {
+    public static void main(String[] args) {
+        int temperature = 25;
 
-if (temperature > 35)
-System.out.println("It's too hot outside.");
-else if (temperature > 20)
-System.out.println("The weather is pleasant.");
-else
-System.out.println("It's quite cold.");
-}
+        // Program to Demonstrate Conditional Statements in Java
+        if (temperature > 35) {
+            System.out.println("It's too hot outside.");
+        } else if (temperature > 20) {
+            System.out.println("The weather is pleasant.");
+        } else {
+            System.out.println("It's quite cold.");
+        }
+    }
 }
